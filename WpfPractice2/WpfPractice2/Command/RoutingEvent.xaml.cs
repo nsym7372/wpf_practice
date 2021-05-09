970f99fbe63eb08e7af2dcc77ecbf0825c8b2214
nsym7372 <nsym7372@outlook.com>
2021-05-09 23:19:12 +0900

    itemtemplate

---
+++ WpfPractice2/WpfPractice2/Command/RoutingEvent.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPractice2.Command
+{
+    /// <summary>
+    /// RoutingEvent.xaml の相互作用ロジック
+    /// </summary>
+    public partial class RoutingEvent : Page
+    {
+        public RoutingEvent()
+        {
+            InitializeComponent();
+        }
+
+        private void BtAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var parent = new Person { Name = "parent" };
+            var child = new Person { Name = "child" };
+
+            parent.AddChild(child);
+
+            // parentだけ、イベントを追加
+            parent.ToAge += (object s, RoutedEventArgs e) =>
+            {
+                var message = $"sender : {((Person)s).Name} / name : {((Person)e.Source).Name}";
+                MessageBox.Show(message);
+            };
+
+            // parent にはイベントを登録した
+            parent.RaiseEvent(new RoutedEventArgs(Person.ToAgeEvent));
+            
+            // child　にはイベントを（明示的に）登録していない ＝　トンネルイベントとして登録された
+            child.RaiseEvent(new RoutedEventArgs(Person.ToAgeEvent));
+        }
+    }
+
+    class Person : FrameworkElement
+    {
+        public static RoutedEvent ToAgeEvent = EventManager.RegisterRoutedEvent(
+            "ToAge",
+            RoutingStrategy.Tunnel,
+            typeof(RoutedEventHandler),
+            typeof(Person)
+            );
+
+        public event RoutedEventHandler ToAge
+        {
+            add { this.AddHandler(ToAgeEvent, value); }
+            remove { this.RemoveHandler(ToAgeEvent, value); }
+        }
+
+        public void AddChild(Person child)
+        {
+            this.AddLogicalChild(child);
+        }
+    }
+}
---
+++ WpfPractice2/WpfPractice2/Controls/TemplatesSample.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPractice2.Controls
+{
+    /// <summary>
+    /// TemplatesSample.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TemplatesSample : Page
+    {
+        public TemplatesSample()
+        {
+            InitializeComponent();
+
+            var source = Enumerable.Range(0, 10).Select(r => new ControlsPerson()
+            {
+                Name = $"なまえ {r}",
+                Age = 20 + r
+            });
+
+            listbox.ItemsSource = source;
+
+            listbox1.ItemsSource = source;
+            
+
+        }
+    }
+    public class ControlsPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+
+
+
+}
