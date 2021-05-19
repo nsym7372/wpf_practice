a98edacc640bfb76dddd0054f77b5d09f9e10e89
nsym7372 <nsym7372@outlook.com>
2021-05-19 22:39:59 +0900

    シンプルなprismプロジェクト

---
+++ WpfPractice2/PrismPractice/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Prism.Mvvm;
+
+namespace PrismPractice.ViewModels
+{
+    using Prism.Commands;
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string input;
+        public string Input
+        {
+            get { return input; }
+            set 
+            { 
+                SetProperty(ref this.input, value);
+                this.ConvertCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private string output;
+        public string Output
+        {
+            get { return output; }
+            set { SetProperty(ref this.output, value); }
+        }
+
+        public DelegateCommand ConvertCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            ConvertCommand = new DelegateCommand(
+                ConvertExecute, 
+                CanConvertExecute
+                );
+        }
+
+        private void ConvertExecute()
+        {
+            this.Output = this.Input.ToUpper();
+        }
+
+        private bool CanConvertExecute()
+        {
+            return (string.IsNullOrWhiteSpace(this.Input) == false);
+        }
+    }
+}
